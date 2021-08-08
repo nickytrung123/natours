8e25d63006ed8b6a8b6e614d1078d908442ca87a
LE MINH TRUNG <4401104233@student.hcmup.edu.vn>
2021-08-08 12:53:06 +0700

    require stripe

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,10 +1,11 @@
 /* eslint-disable */
 import axios from 'axios';
-import { loadStripe } from '@stripe/stripe-js';
+const Stripe = require('stripe');
+
+const stripe = Stripe(`${process.env.STRIPE_PUBLIC_KEY}`);
 
 export const bookTour = async (tourId) => {
   try {
-    const stripe = await loadStripe(`${process.env.STRIPE_PUBLIC_KEY}`);
     // 1) Get checkout session from API
     const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
 
