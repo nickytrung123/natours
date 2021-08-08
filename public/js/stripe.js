ec37c9b0ed1093011a2c85c2b750a3d8dbcaca0b
LE MINH TRUNG <4401104233@student.hcmup.edu.vn>
2021-08-08 17:47:09 +0700

    config heroku

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -7,7 +7,8 @@ export const bookTour = async (tourId) => {
   try {
     // 1) Get checkout session from API
     const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+      `/api/v1/bookings/checkout-session/${tourId}`
+      // `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
     );
 
     // 2) Create checkout form + chanre credit card
