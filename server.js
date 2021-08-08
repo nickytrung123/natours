25986355264c3b4a125d382c0740b62ff365464d
LE MINH TRUNG <4401104233@student.hcmup.edu.vn>
2021-08-08 10:22:55 +0700

    config port for server

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose
   .then(() => console.log('DB connection successful 👌'))
   .catch(() => console.log('DB connection failed 💥'));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}✨`);
 });
