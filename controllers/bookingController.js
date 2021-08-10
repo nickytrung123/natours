5ad1a8ec8b61d904e13b1c52e51161f1f1175d77
LE MINH TRUNG <4401104233@student.hcmup.edu.vn>
2021-08-10 10:45:42 +0700

    .

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -58,7 +58,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount;
+  const price = session.amount_total;
   await Booking.create({ tour, user, price });
 };
 
